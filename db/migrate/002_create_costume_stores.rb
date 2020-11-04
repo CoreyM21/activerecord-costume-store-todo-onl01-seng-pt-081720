43f06155b1cbe6bf171a4c5238e1d03ac2b3b7b0
Corey Martin <coreyjmartin2@gmail.com>
2020-11-04 20:00:44 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
         c.string :name
         c.string :location
         c.integer :costume_inventory
-        c.integer :number_of_employees
+        c.integer :num_of_employees
         c.boolean :status
         c.datetime :opening_time
         c.datetime :closing_time 
