215beca1282831756016194a8f73e7cec76ce570
Corey Martin <coreyjmartin2@gmail.com>
2020-11-04 19:51:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
         c.string :name
         c.integer :price
         c.string :image_url
-        c.string :size 
+        c.string :size
+        
       end
     end
     
