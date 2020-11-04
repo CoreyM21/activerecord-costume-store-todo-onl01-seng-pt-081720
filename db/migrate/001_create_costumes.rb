40d37c3b5b940dcaa9e4306c0258f0605a0db72a
Corey Martin <coreyjmartin2@gmail.com>
2020-11-04 19:48:23 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,6 +13,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     def change
       create_table :costumes do |c|
         c.string :name
+        c.integer :price
+        c.string :
         
         
       end
